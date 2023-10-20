3c666a26704747538502e15cce19f735e1c9353f
Chintey Ley <96514533+Chinteyley@users.noreply.github.com>
2023-10-20 09:01:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function setLightMode() {
+    document.body.style.backgroundColor = '#7f7a7d';
+    document.body.style.color = '#e4e2e3';
+}
+
+// Function to switch to dark mode
+function setDarkMode() {
+    document.body.style.backgroundColor = '#393738';
+    document.body.style.color = '#e4e2e3';
+}
